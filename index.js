4eb0ab0adba3bfddb5590bc42ac95655a6366db9
laxmiprasannachukka <prasannagadarla@gmail.com>
2021-05-21 18:18:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const cors = require("cors");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const router = express.Router();
+const path = require("path");
+const jwt =require("jsonwebtoken");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/Rest_API/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/Rest_API/auth", authRoute);
+app.use("/Rest_API/users", userRoute);
+app.use("/Rest_API/posts", postRoute);
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
